2c075797b0137ffc53b3ad38fb60fd19d723d387
Angelo Rafael Peres Pinto <angelo.pinto@invillia.com>
2022-11-09 10:36:26 -0400

    feat: Implementando Tema no Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-100": "#8284FA",
+        "purple-200": "#5E60CE",
+        "blue-100": "#4EA8DE",
+        "blue-200": "#1E6F9F",
+        "grey-100": "#F2F2F2",
+        "grey-200": "#D9D9D9",
+        "grey-300": "#808080",
+        "grey-400": "#333333",
+        "grey-500": "#262626",
+        "grey-600": "#0D0D0D",
+        "grey-700": "#0D0D0D",
+        danger: "#E25858",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
